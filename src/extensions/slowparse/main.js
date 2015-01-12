fe7d64aa1f6bcc5f1ff4bf767168fa39889797f4
Kenny Nguyen <kenimorri@gmail.com>
2015-01-12 11:18:33 -0500

    Added temp code for finding line number

--- src/extensions/slowparse/main.js
+++ src/extensions/slowparse/main.js
@@ -13,7 +13,32 @@ define(function (require, exports, module) {
         var editor = EditorManager.getFocusedEditor();
         var text   = editor.document.getText();
         var result = parser(text);
-        window.alert(result);
+        
+        
+        //TEMP CODE FOR KLEVER
+        //Line counter code
+        var endCount = 0;
+        var linebegin = 0;
+        var text2;
+        for(var i = 0; i <= (result[2] - 1); i++)
+        {
+            if(text[i] == "\n")
+            {
+                endCount += 1;
+                linebegin = i;
+            }
+            text2 += text[i];
+        }
+        //window.alert("EndLine Equals: " + endCount +"\n Line Begin was: " + linebegin + "\n result was: " + result[1] + "\n" + text2);
+        console.log("EndLine Equals: " + endCount +"\n Line Begin was: " + linebegin + "\n result was: " + result[1] + "\n" + text2);
+        console.log("Charater at Result was: " + text2[result[1]] + " " + text2[result[2]]);
+        console.log("Character at linebegin was: " + text2[linebegin]);
+        console.log(text2[196] + text2[197] + text2[198] + text2[199] + text2[200] + text2[201] + text2[202] + text2[203] + text2[204]);
+        //END TEMP CODE
+        
+        
+        
+        
     }
     // First, register a command - a UI-less object associating an id to a handler
     var MY_COMMAND_ID = "slowparse"; // package-style naming to avoid collisions
