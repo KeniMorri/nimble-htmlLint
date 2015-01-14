8972f57b546af10fd1db0fa9b672fbba57cbeaae
Andrew Benner <ajdbenner@hotmail.com>
2015-01-14 13:48:55 -0500

    Fix #8 Making error message more helpful

--- src/extensions/slowparse/parser.js
+++ src/extensions/slowparse/parser.js
@@ -30,35 +30,35 @@ define(function(require){
                 "UNSUPPORTED_ATTR_NAMESPACE": "<p>The attribute uses an attribute namespace that is not permitted under HTML5 conventions.</p>",
                 "MULTIPLE_ATTR_NAMESPACES": "<p>The attribute has multiple namespaces. Check your text and make sure there's only a single namespace prefix for the attribute.</p>",
                 "INVALID_CSS_DECLARATION": "<p>This CSS declaration never closes.</p>",
-/*Left off here*/"INVALID_CSS_PROPERTY_NAME": "<p>CSS property [[result.error.cssProperty.property]] does not exist. You may want to see a <a href='https://developer.mozilla.org/en/CSS/CSS_Reference'>list of CSS properties</a>.</p>",
-                "INVALID_CSS_RULE": "<p><em data-highlight='[[result.error.cssRule.start]],[[result.error.cssRule.end]]'>This</em> CSS rule is not legal CSS.</p>",
-                "INVALID_TAG_NAME": "<p>The <code>&lt;</code> character <em data-highlight='[[result.error.openTag.start]],[[result.error.openTag.end]]'>here</em> appears to be the beginning of a tag, but is not followed by a valid tag name.</p> <p>If you just want a <code>&lt;</code> to appear on your Web page, try using <code>&amp;lt;</code> instead.</p> <p>Or, see a <a href='https://developer.mozilla.org/en/docs/Web/Guide/HTML/HTML5/HTML5_element_list'>list of HTML5 tags</a>.</p>",
-                "JAVASCRIPT_URL_NOT_ALLOWED": "<p>Sorry, but security restrictions on this site prevent you from using the <code>javascript:</code> URL <em data-highlight='[[result.error.value.start]],[[result.error.value.end]]'>here</em>. If you really need to use JavaScript, consider using <a href='http://jsbin.com/'>jsbin</a> or <a href='http://jsfiddle.net/'>jsfiddle</a>.</p>",
-                "MISMATCHED_CLOSE_TAG": "<p>The closing <code>&lt;/[[result.error.closeTag.name]]&gt;</code> tag <em data-highlight='[[result.error.closeTag.start]],[[result.error.closeTag.end]]'>here</em> doesn't pair with the opening <code>&lt;[[result.error.openTag.name]]&gt;</code> tag <em data-highlight='[[result.error.openTag.start]],[[result.error.openTag.end]]'>here</em>. This is likely due to a missing or misplaced <code>&lt;/[[result.error.openTag.name]]&gt;</code> tag.",
-                "MISSING_CSS_BLOCK_CLOSER": "<p>Missing block closer or next property:value; pair following <em data-highlight='[[result.error.cssValue.start]],[[result.error.cssValue.end]]'>[[result.error.cssValue.value]]</em>.</p>",
-                "MISSING_CSS_BLOCK_OPENER": "<p>Missing block opener after <em data-highlight='[[result.error.cssSelector.start]],[[result.error.cssSelector.end]]'>[[result.error.cssSelector.selector]]</em>.</p>",
-                "MISSING_CSS_PROPERTY": "<p>Missing property for <em data-highlight='[[result.error.cssSelector.start]],[[result.error.cssSelector.end]]'>[[result.error.cssSelector.selector]]</em>.</p>",
-                "MISSING_CSS_SELECTOR": "<p>Missing either a new CSS selector or the &lt;/style&gt; tag <em data-highlight='[[result.error.cssBlock.start]],[[result.error.cssBlock.end]]'>here</em>.</p>",
-                "MISSING_CSS_VALUE": "<p>Missing value for <em data-highlight='[[result.error.cssProperty.start]],[[result.error.cssProperty.end]]'>[[result.error.cssProperty.property]]</em>.</p>",
-                "SCRIPT_ELEMENT_NOT_ALLOWED": "<p>Sorry, but security restrictions on this site prevent you from using <code>&lt;script&gt;</code> tags <em data-highlight='[[result.error.openTag.start]],[[result.error.closeTag.end]]'>here</em>. If you really need to use JavaScript, consider using <a href='http://jsbin.com/'>jsbin</a> or <a href='http://jsfiddle.net/'>jsfiddle</a>.</p>",
-                "SELF_CLOSING_NON_VOID_ELEMENT": "<p>The <code>&lt;[[result.error.name]]&gt;</code> tag <em data-highlight='[[result.error.start]],[[result.error.end]]'>here</em> can't be self-closed, because <code>&lt;[[result.error.name]]&gt;</code> is not a void element; it must be closed with a separate <code>&lt;/[[result.error.name]]&gt;</code> tag.</p>",
-                "UNCAUGHT_CSS_PARSE_ERROR": "<p>A parse error occurred outside expected cases: <em data-highlight='[[result.error.error.start]],[[result.error.error.end]]'>[[result.error.error.msg]]</em></p>",
-                "UNCLOSED_TAG": "<p>The <code>&lt;[[result.error.openTag.name]]&gt;</code> tag <em data-highlight='[[result.error.openTag.start]],[[result.error.openTag.end]]'>here</em> never closes.</p>",
-                "UNEXPECTED_CLOSE_TAG": "<p>The closing <code>&lt;/[[result.error.closeTag.name]]&gt;</code> tag <em data-highlight='[[result.error.closeTag.start]],[[result.error.closeTag.end]]'>here</em> doesn't pair with anything, because there are no opening tags that need to be closed.</p>",
-                "UNFINISHED_CSS_PROPERTY": "<p>Property <em data-highlight='[[result.error.cssProperty.start]],[[result.error.cssProperty.end]]'>[[result.error.cssProperty.property]]</em> still needs finalizing with :</p>",
-                "UNFINISHED_CSS_SELECTOR": "<p>Selector <em data-highlight='[[result.error.cssSelector.start]],[[result.error.cssSelector.end]]'>[[result.error.cssSelector.selector]]</em> still needs finalizing with {</p>",
-                "UNFINISHED_CSS_VALUE": "<p>Value <em data-highlight='[[result.error.cssValue.start]],[[result.error.cssValue.end]]'>[[result.error.cssValue.value]]</em> still needs finalizing with ;</p>",
-                "UNKOWN_CSS_KEYWORD": "<p>The CSS @keyword <em data-highlight='[[result.error.cssKeyword.start]],[[result.error.cssKeyword.end]]'>[[result.error.cssKeyword.value]]</em> does not match any known @keywords.</p>",
-                "UNQUOTED_ATTR_VALUE": "<p>The Attribute value <em data-highlight='[[result.error.start]]'>here</em> should start with an opening double quote.</p>",
-                "UNTERMINATED_ATTR_VALUE": "<p>The <code>&lt;[[result.error.openTag.name]]&gt;</code> tag's <code>[[result.error.attribute.name.value]]</code> attribute has a value <em data-highlight='[[result.error.attribute.value.start]]'>here</em> that doesn't end with a closing double quote.</p>",
-                "UNTERMINATED_CLOSE_TAG": "<p>The closing <code>&lt;/[[result.error.closeTag.name]]&gt;</code> tag <em data-highlight='[[result.error.closeTag.start]],[[result.error.closeTag.end]]'>here</em> doesn't end with a <code>&gt;</code>.</p>",
-                "UNTERMINATED_COMMENT": "<p>The comment <em data-highlight='[[result.error.start]]'>here</em> doesn't end with a <code>--&gt;</code>.</p>",
-                "UNTERMINATED_CSS_COMMENT": "<p>The CSS comment <em data-highlight='[[result.error.start]]'>here</em> doesn't end with a <code>*/</code>.</p>",
-                "UNBOUND_ATTRIBUTE_VALUE": "<p>The attribute value <code>[[result.error.value]]</code> <em data-highlight='[[result.error.interval.start]],[[result.error.interval.end]]'>here</em> appears to be detached from an attribute. You may be missing an '=' sign.</p>",
-                "UNTERMINATED_OPEN_TAG": "<p>The opening <code>&lt;[[result.error.openTag.name]]&gt;</code> tag <em data-highlight='[[result.error.openTag.start]],[[result.error.openTag.end]]'>here</em> doesn't end with a <code>&gt;</code>.</p>"
+				"INVALID_CSS_PROPERTY_NAME": "<p>CSS property <em>{{ error }}</em> does not exist. You may want to see a <a href='https://developer.mozilla.org/en/CSS/CSS_Reference'>list of CSS properties</a>.</p>",
+                "INVALID_CSS_RULE": "<p>This CSS rule is not legal CSS.</p>",
+                "INVALID_TAG_NAME": "<p>The <code>&lt;</code> character appears to be the beginning of a tag, but is not followed by a valid tag name.</p> <p>If you just want a <code>&lt;</code> to appear on your Web page, try using <code>&amp;lt;</code> instead.</p> <p>Or, see a <a href='https://developer.mozilla.org/en/docs/Web/Guide/HTML/HTML5/HTML5_element_list'>list of HTML5 tags</a>.</p>",
+                "JAVASCRIPT_URL_NOT_ALLOWED": "<p>Sorry, but security restrictions on this site prevent you from using the <code>javascript:</code> URL. If you really need to use JavaScript, consider using <a href='http://jsbin.com/'>jsbin</a> or <a href='http://jsfiddle.net/'>jsfiddle</a>.</p>",
+                "MISMATCHED_CLOSE_TAG": "<p>The closing <code>&lt;/{{ error }}&gt;</code> tag doesn't pair with the opening <code>&lt;{{ error1 }}&gt;</code> tag. This is likely due to a missing or misplaced <code>&lt;/{{ error1 }}&gt;</code> tag.",
+				"MISSING_CSS_BLOCK_CLOSER": "<p>Missing block closer or next property:value; pair following <em>{{ error }}</em>.</p>",
+                "MISSING_CSS_BLOCK_OPENER": "<p>Missing block opener after <em>{{ error }}</em>.</p>",
+                "MISSING_CSS_PROPERTY": "<p>Missing property for <em>{{ error }}</em>.</p>",
+                "MISSING_CSS_SELECTOR": "<p>Missing either a new CSS selector or the &lt;/style&gt; tag.</p>",
+                "MISSING_CSS_VALUE": "<p>Missing value for <em>{{ error }}</em>.</p>",
+                "SCRIPT_ELEMENT_NOT_ALLOWED": "<p>Sorry, but security restrictions on this site prevent you from using <code>&lt;script&gt;</code> tags. If you really need to use JavaScript, consider using <a href='http://jsbin.com/'>jsbin</a> or <a href='http://jsfiddle.net/'>jsfiddle</a>.</p>",
+                "SELF_CLOSING_NON_VOID_ELEMENT": "<p>The <code>&lt;{{ error }}&gt;</code> tag can't be self-closed, because <code>&lt;{{ error }}&gt;</code> is not a void element; it must be closed with a separate <code>&lt;/{{ error }}&gt;</code> tag.</p>",
+                "UNCAUGHT_CSS_PARSE_ERROR": "<p>A parse error occurred outside expected cases: <em>{{ error }}</em></p>",
+                "UNCLOSED_TAG": "<p>The <code>&lt;{{ error }}&gt;</code> tag never closes.</p>",
+				"UNEXPECTED_CLOSE_TAG": "<p>The closing <code>&lt;/{{ error }}&gt;</code> tag doesn't pair with anything, because there are no opening tags that need to be closed.</p>",
+                "UNFINISHED_CSS_PROPERTY": "<p>Property <em>{{ error }}</em> still needs finalizing with :</p>",
+                "UNFINISHED_CSS_SELECTOR": "<p>Selector <em>{{ error }}</em> still needs finalizing with {</p>",
+                "UNFINISHED_CSS_VALUE": "<p>Value <em>{{ error }}</em> still needs finalizing with ;</p>",
+                "UNKOWN_CSS_KEYWORD": "<p>The CSS @keyword <em>{{ error }}</em> does not match any known @keywords.</p>",
+                "UNQUOTED_ATTR_VALUE": "<p>The Attribute value should start with an opening double quote.</p>",
+                "UNTERMINATED_ATTR_VALUE": "<p>The <code>&lt;{{ error }}&gt;</code> tag's <code>{{ error1 }}</code> attribute has a value that doesn't end with a closing double quote.</p>",
+                "UNTERMINATED_CLOSE_TAG": "<p>The closing <code>&lt;/{{ error }}&gt;</code> tag doesn't end with a <code>&gt;</code>.</p>",
+                "UNTERMINATED_COMMENT": "<p>The comment doesn't end with a <code>--&gt;</code>.</p>",
+                "UNTERMINATED_CSS_COMMENT": "<p>The CSS comment doesn't end with a <code>*/</code>.</p>",
+                "UNBOUND_ATTRIBUTE_VALUE": "<p>The attribute value <code>{{ error }}</code> appears to be detached from an attribute. You may be missing an '=' sign.</p>",
+                "UNTERMINATED_OPEN_TAG": "<p>The opening <code>&lt;{{ error }}&gt;</code> tag doesn't end with a <code>&gt;</code>.</p>"
             };
             
-            //human-readable msg, start, end of error based on error type
+            //human-readable msg, start, and end of error based on error type
             if (obj.type === "ATTRIBUTE_IN_CLOSING_TAG"){
                 msg[0] = Mustache.render(errorJSON.ATTRIBUTE_IN_CLOSING_TAG, { 'error': obj.closeTag.name });
                 msg[1] = obj.closeTag.start;
@@ -83,7 +83,7 @@ define(function(require){
                 msg[2] = obj.html.end;
             }
             if (obj.type === "HTTP_LINK_FROM_HTTPS_PAGE"){ 
-                msg[0] = Mustache.render(errorJSON.HTTP_LINK_FROM_HTTPS_PAGE, { 'error': obj.openTag.name, 'error1': obj.attribute.name.value});
+                msg[0] = Mustache.render(errorJSON.HTTP_LINK_FROM_HTTPS_PAGE, { 'error': obj.openTag.name, 'error1': obj.attribute.name.value });
                 msg[1] = obj.openTag.start;
                 msg[2] = obj.openTag.end;
             }
@@ -106,7 +106,7 @@ define(function(require){
                 msg[0] = errorJSON.INVALID_CSS_DECLARATION;
             }
             if (obj.type === "INVALID_CSS_PROPERTY_NAME"){
-                msg[0] = errorJSON.INVALID_CSS_PROPERTY_NAME;
+                msg[0] = Mustache.render(errorJSON.INVALID_CSS_PROPERTY_NAME, { 'error': obj.cssProperty.property });
                 msg[1] = obj.cssProperty.start;
                 msg[2] = obj.cssProperty.end;
             }
@@ -122,22 +122,22 @@ define(function(require){
                 msg[0] = errorJSON.JAVASCRIPT_URL_NOT_ALLOWED;
             }
             if (obj.type === "MISMATCHED_CLOSE_TAG"){
-                msg[0] = errorJSON.MISMATCHED_CLOSE_TAG;
+                msg[0] = Mustache.render(errorJSON.MISMATCHED_CLOSE_TAG, { 'error': obj.closeTag.name, 'error1': obj.openTag.name });
                 msg[1] = obj.closeTag.start;
                 msg[2] = obj.closeTag.end;
             }
             if (obj.type === "MISSING_CSS_BLOCK_CLOSER"){
-                msg[0]= errorJSON.MISSING_CSS_BLOCK_CLOSER;
+                msg[0] = Mustache.render(errorJSON.MISSING_CSS_BLOCK_CLOSER, { 'error': obj.cssValue.value });
                 msg[1] = obj.cssValue.start;
                 msg[2] = obj.cssValue.end;
             }
             if (obj.type === "MISSING_CSS_BLOCK_OPENER"){
-                msg[0] = errorJSON.MISSING_CSS_BLOCK_OPENER;
+                msg[0] = Mustache.render(errorJSON.MISSING_CSS_BLOCK_OPENER, { 'error': obj.cssSelector.selector });
                 msg[1] = obj.cssSelector.start;
                 msg[2] = obj.cssSelector.end;
             }
             if (obj.type === "MISSING_CSS_PROPERTY"){
-                msg[0] = errorJSON.MISSING_CSS_PROPERTY;
+                msg[0] = Mustache.render(errorJSON.MISSING_CSS_PROPERTY, { 'error': obj.cssSelector.selector});
                 msg[1] = obj.cssSelector.start;
                 msg[2] = obj.cssSelector.end;
             }
@@ -147,7 +147,7 @@ define(function(require){
                 msg[2] = obj.cssBlock.end;
             }
             if (obj.type === "MISSING_CSS_VALUE"){
-                msg[0] = errorJSON.MISSING_CSS_VALUE;
+                msg[0] = Mustache.render(errorJSON.MISSING_CSS_VALUE, { 'error': obj.cssProperty.property});
                 msg[1] = obj.cssProperty.start;
                 msg[2] = obj.cssProperty.end;
             }
@@ -155,42 +155,42 @@ define(function(require){
                 msg[0] = errorJSON.SCRIPT_ELEMENT_NOT_ALLOWED;
             }
             if (obj.type === "SELF_CLOSING_NON_VOID_ELEMENT"){
-                msg[0] = errorJSON.SELF_CLOSING_NON_VOID_ELEMENT;
+                msg[0] = Mustache.render(errorJSON.SELF_CLOSING_NON_VOID_ELEMENT, { 'error': obj.error.name});
                 msg[1] = obj.start;
                 msg[2] = obj.end;
             }
             if (obj.type === "UNCAUGHT_CSS_PARSE_ERROR"){
-                msg[0] = errorJSON.UNCAUGHT_CSS_PARSE_ERROR;
+                msg[0] = Mustache.render(errorJSON.UNCAUGHT_CSS_PARSE_ERROR, { 'error': obj.error.msg});
                 msg[1] = obj.error.start;
                 msg[2] = obj.error.end;
             }
             if (obj.type === "UNCLOSED_TAG"){
-                msg[0] = errorJSON.UNCLOSED_TAG;
+                msg[0] = Mustache.render(errorJSON.UNCLOSED_TAG, { 'error': obj.openTag.name});
                 msg[1] = obj.openTag.start;
                 msg[2] = obj.openTag.end;
             }
             if (obj.type === "UNEXPECTED_CLOSE_TAG"){
-                msg[0] = errorJSON.UNEXPECTED_CLOSE_TAG;
+                msg[0] = Mustache.render(errorJSON.UNEXPECTED_CLOSE_TAG, { 'error': obj.closeTag.name });
                 msg[1] = obj.closeTag.start;
                 msg[2] = obj.closeTag.end;
             }
             if (obj.type === "UNFINISHED_CSS_PROPERTY"){
-                msg[0] = errorJSON.UNFINISHED_CSS_PROPERTY;
+                msg[0] = Mustache.render(errorJSON.UNFINISHED_CSS_PROPERTY, { 'error': obj.cssProperty.property });
                 msg[1] = obj.cssProperty.start;
                 msg[2] = obj.cssProperty.end;
             }
             if (obj.type === "UNFINISHED_CSS_SELECTOR"){
-                msg[0] = errorJSON.UNFINISHED_CSS_SELECTOR;
+                msg[0] = Mustache.render(errorJSON.UNFINISHED_CSS_SELECTOR, { 'error': obj.cssSelector.selector });
                 msg[1] = obj.cssSelector.start;
                 msg[2] = obj.cssSelector.end;
             }
             if (obj.type === "UNFINISHED_CSS_VALUE"){
-                msg[0] = errorJSON.UNFINISHED_CSS_VALUE;
+                msg[0] = Mustache.render(errorJSON.UNFINISHED_CSS_VALUE, { 'error': obj.cssValue.value });
                 msg[1] = obj.cssValue.start;
                 msg[2] = obj.cssValue.end;
             }
             if (obj.type === "UNKOWN_CSS_KEYWORD"){
-                msg[0] = errorJSON.UNKOWN_CSS_KEYWORD;
+                msg[0] = Mustache.render(errorJSON.UNKOWN_CSS_KEYWORD, { 'error': obj.cssKeyword.value });
                 msg[1] = obj.cssKeyword.start;
                 msg[2] = obj.cssKeyword.end;
             }
@@ -200,12 +200,12 @@ define(function(require){
                 msg[2] = obj.start;
             }
             if (obj.type === "UNTERMINATED_ATTR_VALUE"){
-                msg[0] = errorJSON.UNTERMINATED_ATTR_VALUE;
+                msg[0] = Mustache.render(errorJSON.UNTERMINATED_ATTR_VALUE, { 'error': obj.openTag.name, 'error1': obj.attribute.name.value });
                 msg[1] = obj.openTag.start;
                 msg[2] = obj.openTag.end;
             }
             if (obj.type === "UNTERMINATED_CLOSE_TAG"){
-                msg[0] = errorJSON.UNTERMINATED_CLOSE_TAG;
+                msg[0] = Mustache.render(errorJSON.UNTERMINATED_CLOSE_TAG, { 'error': obj.closeTag.name });
                 msg[1] = obj.closeTag.start;
                 msg[2] = obj.closeTag.end;
             }
@@ -220,12 +220,12 @@ define(function(require){
                 msg[2] = obj.start;
             }
             if (obj.type === "UNBOUND_ATTRIBUTE_VALUE"){
-                msg[0] = errorJSON.UNBOUND_ATTRIBUTE_VALUE;
+                msg[0] = Mustache.render(errorJSON.UNBOUND_ATTRIBUTE_VALUE, { 'error': obj.value });
                 msg[1] = obj.interval.start;
                 msg[2] = obj.interval.end;
             }
             if (obj.type === "UNTERMINATED_OPEN_TAG"){
-                msg[0] = errorJSON.UNTERMINATED_OPEN_TAG;
+                msg[0] = Mustache.render(errorJSON.UNTERMINATED_OPEN_TAG, { 'error': obj.openTag.name });
                 msg[1] = obj.openTag.start;
                 msg[2] = obj.openTag.end;
             }
@@ -259,7 +259,7 @@ define(function(require){
                 parsedText += input[i];
                 charCount++;
             }
-            //character relative to start of the row
+            //character relative to start of the line
             msg[1] = msg[1] - lineBeginStart
             msg[2] = msg[2] - lineBeginEnd;
             
